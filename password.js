c5637f4a10961c3f440ede24df83c58a7eaa7957
CSH <131988713+chosoho1@users.noreply.github.com>
2024-05-02 00:52:09 +0900

    Add files via upload

---
+++ password.js
@@ -0,0 +1,11 @@
+
+var password = "8881"
+ function passcheck() {
+if(document.getElementById("8881"). value != password){
+alert("삐삐삐삐삐삐삐삐삐삐삐");
+return false;
+}
+if(document.getElementById("8881"). value == password){
+alert("삐로리");
+}
+ }   
